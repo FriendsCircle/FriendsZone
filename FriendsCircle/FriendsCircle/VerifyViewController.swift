0eba467c9634d5cb2f5551e6aa0d809d78863426
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2016-08-19 19:23:29 +0700

    test commit

--- FriendsCircle/FriendsCircle/VerifyViewController.swift
+++ FriendsCircle/FriendsCircle/VerifyViewController.swift
@@ -21,6 +21,8 @@ class VerifyViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
     
 
