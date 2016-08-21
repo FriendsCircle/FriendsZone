678d173f1a517c1ad8e7e5b63e9cee3ed47a1654
Huỳnh Trí Dũng <dunghuynh1990@gmail.com>
2016-08-21 11:40:19 +0700

    update login view with new UI

--- FriendsCircle/FriendsCircle/LoginViewController.swift
+++ FriendsCircle/FriendsCircle/LoginViewController.swift
@@ -4,7 +4,9 @@
 //
 //  Created by Vu Nguyen on 8/13/16.
 //  Copyright © 2016 Huynh Tri Dung. All rights reserved.
-//
+//  TODO: Check login logic
+//  TODO: check textfield logic
+
 
 import UIKit
 import Contacts
@@ -20,8 +22,6 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         phoneNumTxtField.text = "+84937264497"
-
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func onLogin(sender: UIButton) {
@@ -29,19 +29,12 @@ class LoginViewController: UIViewController {
         if let phone = phoneNumTxtField.text {
             getVerifyPhoneNumber(phone)
         }
-        
         performSegueWithIdentifier("verifySegue", sender: self)
-    }
-    
-    @IBAction func onFetchButton(sender: UIButton) {
-        //fetchContact()
-        //performSegueWithIdentifier("Login2ContactsList", sender: self)
     }
     
     func getVerifyPhoneNumber(phone:String) {
         
         let loginClient = LoginClient()
-        //        let userRef = loginClient.getRefFirebaseByPhoneNumber(phone)
         loginClient.getVerifyPhoneNumber({ () -> () in
             print("I get verify in")
             //self.performSegueWithIdentifier("verifySegue", sender: nil)
@@ -50,26 +43,12 @@ class LoginViewController: UIViewController {
             }, phone: phone)
     }
     
-    //This function gets all the contact from iPhone. Currently, it gets first name, last name and phonenumber. With phonenumber, it gets phone in field CNLabelPhoneNumberMobile, it needs to be more investigated to get true mobilephone number
-    
-    
-    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        if segue.identifier == "Login2ContactsList" {
-            let nextVC = segue.destinationViewController as! ContactsListViewController
-            
-        }
-        if segue.identifier == "verifySegue" {
             let verifyVC = segue.destinationViewController as! VerifyViewController
             verifyVC.phoneNum = phoneNumTxtField.text!
-            
-        }
-        
-        // Pass the selected object to the new view controller.
     }
     
 
