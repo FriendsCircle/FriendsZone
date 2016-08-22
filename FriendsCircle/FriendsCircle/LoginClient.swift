ddff230b1bd3db20689b81b8559634ccbd7e080e
Le Huynh Anh Tien <atien.lh@gmail.com>
2016-08-22 11:23:07 +0700

    Update LoginClient.swift
    Add link to get verify number from heroku

--- FriendsCircle/FriendsCircle/LoginClient.swift
+++ FriendsCircle/FriendsCircle/LoginClient.swift
@@ -22,7 +22,8 @@ class LoginClient {
         
     }
     func getVerifyPhoneNumber(success: () -> (), failure: (String) -> (), phone: String, name: String) {
-        
+        // add link to server heroku for get verify number 
+        // https://whispering-stream-37719.herokuapp.com/test
         Alamofire.request(.GET, "http://localhost:3000/test", parameters: ["phone": phone]) .responseJSON { response in
             if ((response.response) != nil) {
                 if let res: NSHTTPURLResponse = response.response! {
